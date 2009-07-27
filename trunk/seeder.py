8791a777afe2d9ddc11bc21b1f163ab421e7235c
frenzo.panettieri <frenzo.panettieri@70dfc250-7862-11de-a588-638aa1453bf3>
2009-07-27 22:24:27 +0000

    Basic seeder functionality

--- trunk/seeder.py
+++ trunk/seeder.py
@@ -19,14 +19,21 @@ class Seeder:
 		request = re.sub(bbox_str, "", request)
 		
 		# Parse bbox
-		bbox = BBox(bbox_str)
+		bbox = BBox()
+		bbox.parse(bbox_str)
 		
 		# Generate tiles
 		tilesManager = TilesManager()
-		for i in range(zoom):
+		for i in xrange(zoom):
 			for current_bbox in bbox.zoom(i):
 				 full_request = request + current_bbox
 				 tilesManager.getTile(full_request)
+	
+	
+	def getBBOX(self, request):
+		bbox_str = re.search("BBOX=.*?(&|$)", request, re.IGNORECASE).group()[5:]
+		return urllib.unquote(bbox_str).split(',')
+
 	
 	def clean(self, request, parameters):
 		pass
