b41e916bbebbe45cab97f6ec0e8c3fe11878f2ee
frenzo.panettieri <frenzo.panettieri@70dfc250-7862-11de-a588-638aa1453bf3>
2009-10-13 23:15:51 +0000

    Problem fixed when the userdont have permisson to write the pid

--- trunk/pid.py
+++ trunk/pid.py
@@ -3,11 +3,15 @@ from constants.general import PID_FILE
 import os
 import platform
 
+from logger import Logger
+
 def writePID():
 	system_name = platform.system().lower()
 
 	if "linux" in system_name:
-		pid_file = open(PID_FILE, "w")
-		pid_file.write(str(os.getpid()))
-		pid_file.close()
-
+		try:
+			pid_file = open(PID_FILE, "w")
+			pid_file.write(str(os.getpid()))
+			pid_file.close()
+		except:
+			Logger().warning("PID couldn't be written")
